6d2f19f3cc6813767def06193c8637dc40498b7c
t1pson86 <kzoginaue666@gmail.com>
2025-09-12 18:24:29 +0300

    Routers in routers.py have been updated

--- auth-service/app/api/routers.py
+++ auth-service/app/api/routers.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter
 
-from .v1.endpoints import auth
+from .v1.endpoints import auth, users
 
 auth_router = APIRouter(
-    prefix="/api/v1",
-    tags=["Auth Service"]
+    prefix="/api/v1"
 )
 
 auth_router.include_router(
     router=auth.router,
-    prefix="/auth"
+    prefix="/auth",
+    tags=["Auth Service"]
+)
+
+auth_router.include_router(
+    router=users.router,
+    prefix="/users/data",
+    tags=["Users Data"]
 )

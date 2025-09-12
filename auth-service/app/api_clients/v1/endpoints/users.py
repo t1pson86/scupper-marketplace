04773ab4251d123930b210fc461694049036d4df
t1pson86 <kzoginaue666@gmail.com>
2025-09-12 18:23:35 +0300

    Updated client_api to work with users

--- auth-service/app/api_clients/v1/endpoints/users.py
+++ auth-service/app/api_clients/v1/endpoints/users.py
@@ -2,8 +2,10 @@ from fastapi import APIRouter, Depends
 
 from schemas import UserResponse
 from dependencies import TokenDep
+from repositories import UserRepository
 
 router = APIRouter()
+
 
 @router.get('')
 async def user_verification(
@@ -11,3 +13,14 @@ async def user_verification(
 ) -> UserResponse:
     
     return current_user
+
+
+@router.get('/{user_id}')
+async def get_user_by_id(
+    user_id: int,
+    users_repository: UserRepository = Depends()
+) -> UserResponse:
+    
+    return await users_repository.read(
+        id=user_id
+    )

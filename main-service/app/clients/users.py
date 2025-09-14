42743b4930c4d67486623d358ce8512b2a08a024
t1pson86 <kzoginaue666@gmail.com>
2025-09-14 11:37:44 +0300

    Updated the users.py file to work with auth-service

--- main-service/app/clients/users.py
+++ main-service/app/clients/users.py
@@ -19,6 +19,18 @@ class UsersClient:
                     
                     if response.status == 200:
                         return await response.json()
+                    
+                    if response.status == 404:
+                        raise HTTPException(
+                            status_code=status.HTTP_404_NOT_FOUND,
+                            detail="The user was not found"
+                        )
+        
+                    if response.status == 400:
+                        raise HTTPException(
+                            status_code=status.HTTP_400_BAD_REQUEST, 
+                            detail="Inactive user"
+                        )
 
                     raise HTTPException(
                         status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,

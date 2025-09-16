52d9e72c5d16a2c8fae52724dbd9f4c6d931d823
t1pson86 <kzoginaue666@gmail.com>
2025-09-16 19:37:49 +0300

    Changed paths

--- main-service/app/schemas/__init__.py
+++ main-service/app/schemas/__init__.py
@@ -2,3 +2,5 @@ from .users import UserResponse
 from .advertisement import AdvertisementCreate, AdvertisementResponse, AdvertisementPaginationResponse, AdvertisementUpdate, AdvertisementReviewResponse
 from .paginated import PaginatedResponse
 from .reviews import ReviewCreate, ReviewResponse
+from .carts import CartsBase, CartsCreate, CartsResponse
+from .associative import AssociativeResponse

1a2595edb0850bde3cbb43f584975a1055432f48
t1pson86 <kzoginaue666@gmail.com>
2025-09-20 18:05:41 +0300

    updated the carts service

--- main-service/app/services/carts.py
+++ main-service/app/services/carts.py
@@ -36,6 +36,26 @@ class CartsService:
             )
         
         return None
+
+    # async def get_cart_by_user_id(
+    #     self,
+    #     user_id: int
+    # ) -> CartsModel:
+        
+    #     current_cart = await self.session.execute(
+    #         select(CartsModel)
+    #         .where(CartsModel.user_id==user_id)
+    #     )
+
+    #     result = current_cart.scalar_one_or_none()
+
+    #     if result:
+    #         raise HTTPException(
+    #             status_code=status.HTTP_400_BAD_REQUEST,
+    #             detail="You already have a cart"
+    #         )
+        
+    #     return None
         
 
 
@@ -90,8 +110,46 @@ class CartsService:
         
 
     async def delete_ad_on_cart(
-        self
-    ):
-        pass
+        self,
+        user_id: int,
+        advertisement_id: int
+    ) -> dict:
+        
+        existing_cart = await self.session.execute(
+            select(CartsModel)
+            .where(CartsModel.user_id==user_id)
+        )
+
+        current_cart = existing_cart.scalar_one_or_none()
+
+        if current_cart is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="You already have a cart"
+            )
+
+        existing_advertisement = await self.session.execute(
+            select(CartsAdvertisementsModel)
+            .where(
+                CartsAdvertisementsModel.cart_id == current_cart.id,
+                CartsAdvertisementsModel.advertisement_id == advertisement_id
+            )
+        )
+
+        result = existing_advertisement.scalar_one_or_none()
+
+        if result is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Advertisement is not found"
+            )
+        
+        await self.session.delete(result)
+        await self.session.commit()
+
+        return {"delete": True}
+
+
+
 
 
